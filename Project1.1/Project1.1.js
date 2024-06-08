956435fbff5589788abfdbe1cd251587ff3a2cde
Nxwfel <151463675+Nxwfel@users.noreply.github.com>
2024-06-08 15:27:26 +0200

    Add files via upload
    Just testing the ability to code a design (inspired by a design from 99dribble)

---
+++ Project1.1/Project1.1.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    reset: true,
+    distance :'40px',
+    duration:1000,
+    delay:100
+});
+ScrollReveal().reveal('.title',{delay:500,origin:'left'});
+ScrollReveal({
+    reset: true,
+    distance :'-30px',
+    duration:1500,
+    delay:100
+});
+ScrollReveal().reveal('.box',{delay:500,origin:'right'});
